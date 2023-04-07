379c7ab76d4c0fbfc83dbaa56782ef318a94756e
Anand Desai <desaiaj@rose-hulman.edu>
2023-04-07 03:38:22 -0400

    added popup headline+emoji and css theming. Now switches to darkmode according to user's set prefs

--- src/popup/headline.ts
+++ src/popup/headline.ts
@@ -1,5 +1,12 @@
 import headlinePhrases from "./headline-phrases.json";
 
+const headlineContainer: HTMLDivElement = <HTMLDivElement>(
+  document.querySelector("div#headline-container")
+);
+
+const headlineTemplate: HTMLTemplateElement = <HTMLTemplateElement>(
+  document.querySelector("template#headline")
+);
 interface ToggledOffPhrase {
   text: string | null;
   webp: string;
@@ -18,6 +25,95 @@ interface ToggledOnPhrase extends ToggledOffPhrase {
 const toggledOffPhrases: ToggledOffPhrase[] = headlinePhrases["toggled-off"];
 const toggledOnPhrases: ToggledOnPhrase[] = headlinePhrases["toggled-on"];
 
-export function getHeadlinePhrase(isToggled: boolean): string {
-  return "";
+function getToggledOnPhrase(): ToggledOnPhrase {
+  const idx = Math.floor(Math.random() * toggledOnPhrases.length);
+  return toggledOnPhrases[idx]!!;
 }
+
+function getToggledOffPhrase(): ToggledOffPhrase {
+  const idx = Math.floor(Math.random() * toggledOffPhrases.length);
+  return toggledOffPhrases[idx]!!;
+}
+
+function createHeadline(isToggled: boolean): void {
+  const template = headlineTemplate.content.cloneNode(true) as HTMLElement;
+  const picSrc = template.querySelector(
+    "picture.emoji>source"
+  ) as HTMLSourceElement;
+  const picImg = template.querySelector(
+    "picture.emoji>img"
+  ) as HTMLImageElement;
+  const headlineSpan = template.querySelector(
+    "span#headline-text"
+  ) as HTMLSpanElement;
+  if (isToggled) {
+    const emphasizedSpan = document.createElement("span");
+    const phrase = getToggledOnPhrase();
+    emphasizedSpan.classList.add("emphasized-text");
+    emphasizedSpan.textContent = phrase.highlighted;
+
+    headlineSpan.appendChild(emphasizedSpan);
+    headlineSpan.append(phrase.text ?? "");
+    picSrc.srcset = phrase.webp;
+    picImg.src = phrase.gif;
+    picImg.alt = phrase.alt.emoji;
+    picImg.style.width = phrase.alt.width;
+    picImg.style.height = phrase.alt.height;
+  } else {
+    const phrase = getToggledOffPhrase();
+    headlineSpan?.classList.add("deemphasized-text");
+    headlineSpan.textContent = phrase.text;
+    picSrc.srcset = phrase.webp;
+    picImg.src = phrase.gif;
+    picImg.alt = phrase.alt.emoji;
+    picImg.style.width = phrase.alt.width;
+    picImg.style.height = phrase.alt.height;
+  }
+  headlineContainer.appendChild(template);
+}
+function setHeadline(isToggled: boolean): void {
+  const headlineSpan = headlineContainer.querySelector("span#headline-text");
+  if (headlineSpan === null) {
+    console.log("no headlineSpan, creating headline");
+    createHeadline(isToggled);
+  } else {
+    console.log("existing headlineSpan, modifying headline");
+
+    const picSrc = headlineContainer.querySelector(
+      "picture.emoji>source"
+    ) as HTMLSourceElement;
+    const picImg = headlineContainer.querySelector(
+      "picture.emoji>img"
+    ) as HTMLImageElement;
+    if (isToggled) {
+      const phrase = getToggledOnPhrase();
+
+      headlineSpan.classList.remove("deemphasized-text");
+      const emphasizedSpan = document.createElement("span");
+      emphasizedSpan.classList.add("emphasized-text");
+      emphasizedSpan.textContent = phrase.highlighted;
+      headlineSpan.textContent = "";
+      headlineSpan.appendChild(emphasizedSpan);
+      headlineSpan.append(phrase.text ?? "");
+      picSrc.srcset = phrase.webp;
+      picImg.src = phrase.gif;
+      picImg.alt = phrase.alt.emoji;
+      picImg.style.width = phrase.alt.width;
+      picImg.style.height = phrase.alt.height;
+    } else {
+      const phrase = getToggledOffPhrase();
+      headlineSpan.classList.add("deemphasized-text");
+      const emphasizedSpan = headlineSpan.querySelector("span.emphasized-text");
+      if (emphasizedSpan !== null) {
+        headlineSpan.removeChild(emphasizedSpan);
+      }
+      headlineSpan.textContent = phrase.text;
+      picSrc.srcset = phrase.webp;
+      picImg.src = phrase.gif;
+      picImg.alt = phrase.alt.emoji;
+      picImg.style.width = phrase.alt.width;
+      picImg.style.height = phrase.alt.height;
+    }
+  }
+}
+export { setHeadline };
--- src/popup/popup.ts
+++ src/popup/popup.ts
@@ -1,10 +1,7 @@
-import { getHeadlinePhrase } from "./headline";
+import { setHeadline } from "./headline";
 
 const toggleSwitch: HTMLInputElement = <HTMLInputElement>(
   document.querySelector("input#toggle-switch[type=checkbox]")
-);
-const headlineText: HTMLSpanElement = <HTMLSpanElement>(
-  document.querySelector("span#headline-text")
 );
 
 if (toggleSwitch === null) {
@@ -12,16 +9,11 @@ if (toggleSwitch === null) {
     "No input element with id #toggle-switch found in popup.html !"
   );
 } else {
-  setHeadlineText(toggleSwitch.checked);
+  setHeadline(toggleSwitch.checked);
   toggleSwitch.addEventListener("change", onToggle);
 }
 
 function onToggle(event: Event) {
   const toggleSwitch = <HTMLInputElement>event.target;
-  setHeadlineText(toggleSwitch.checked);
-}
-
-function setHeadlineText(isToggled: boolean): void {
-  const phrase = getHeadlinePhrase(isToggled);
-  headlineText.textContent = phrase;
+  setHeadline(toggleSwitch.checked);
 }
